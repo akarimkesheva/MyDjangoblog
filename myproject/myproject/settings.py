ceaae0f4c604b00e95590f3634008f19dbab3f65
akarimkesheva <akarimkesheva@ivytech.edu>
2026-02-19 17:41:49 -0500

    Added static folder and blog.css for real

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
